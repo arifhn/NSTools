8045663b17224b70aa019f5cb3da94d8c641bbf7
arifhn <arif.hn@gmail.com>
2012-01-25 17:26:23 +0700

    add command for deleting config from /datadata (some SGS ROM use that path)

--- reset-config.zip/script/reset.sh
+++ reset-config.zip/script/reset.sh
@@ -1,4 +1,5 @@
 #!/sbin/sh
 mount /dev/block/platform/s3c-sdhci.0/by-name/userdata /data
 rm /data/data/mobi.cyann.nstools/shared_prefs/mobi.cyann.nstools_preferences.xml
+rm /datadata/mobi.cyann.nstools/shared_prefs/mobi.cyann.nstools_preferences.xml
 umount /data
